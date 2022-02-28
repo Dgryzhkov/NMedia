8188b2fc2c3c9f028d0ac68b3632eeaa0b7d92cf
Dgryzhkov <dgryzhkov@gmail.com>
2022-02-28 13:11:14 +0700

    add unLike

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -6,6 +6,7 @@ import ru.netology.nmedia.dto.Post
 interface PostRepository {
     fun getAll(): List<Post>
     fun likeById(id: Long): Post
+    fun unLikeById(id: Long): Post
     fun save(post: Post)
     fun removeById(id: Long)
 }
